da45ac4763ed5672d2683fdeccab375632356925
raghava249 <111784607+raghava249@users.noreply.github.com>
2022-11-01 21:07:06 -0500

    updated step 8

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var EmployeeRouter = require('./routes/Employee');
 var gridbulidRouter = require('./routes/gridbulid');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Employee',EmployeeRouter);
 app.use('/gridbulid',gridbulidRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
